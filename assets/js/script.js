bc242fe93fbc57c45584560d4dd753f7edaeda20
Payton Whinnery <paytoncaliw@gmail.com>
2021-01-26 22:55:04 -0600

    got colors working depending on time and styling is applied

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// var currentHour = new Date().getHours();
+
+
+// for (i=9; i < 18; i++) {
+//         if (i > currentHour) {
+//             $("#" + i).children("textarea").addClass("future")
+//         } else if (i < currentHour) {
+//             $("#" + i).children("textarea").addClass("past")
+//         } else if (i == currentHour) {
+//             $("#" + i).children("textarea").addClass("present")
+//         }
+
+// }
+
+
+
+
